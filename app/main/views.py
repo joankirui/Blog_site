47975b0ec00990d72a0dfbd47c42b438175598f9
joankirui <joankirui99@gmail.com>
2021-11-13 17:37:06 +0300

    create a view function for index.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
 from flask_login import login_required,current_user
+from flask_wtf import form
 from . import main
 from flask import render_template,request,redirect,url_for,abort
 from ..models import User,Blog,Comment,Subscription
-
+from .forms import UpdateBlog,UpdateProfile,NewBlog,MyComment,SubscribeForm
+from .. import db
 
 @main.route('/',methods = ['GET','POST'])
 def index():
@@ -11,6 +13,14 @@ def index():
     # Getting the quotes
     # quotes = get_quotes
 
+    form = SubscribeForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        subscription = Subscription(email = email)
+        db.session.add(subscription)
+        db.session.commit()
+        return render_template('index.html',title = title,blog = blog)
+    return render_template('index.html',title = title,blog = blog)
 @main.route('/new/blog/', methods=['GET','POST'])
 @login_required
 def new_blog():

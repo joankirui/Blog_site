ed824ce8dfd91c3d45edee0b69f5179fb758c476
joankirui <joankirui99@gmail.com>
2021-11-13 16:58:36 +0300

    create a profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,32 @@
-from flask_login import login_required
+from flask_login import login_required,current_user
 from . import main
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User,Blog,Comment,Subscription
+
+
+@main.route('/',methods = ['GET','POST'])
+def index():
+    blog = Blog.query.all()
+    title = "Welcome to Blogsite"
+    # Getting the quotes
+    # quotes = get_quotes
 
 @main.route('/new/blog/', methods=['GET','POST'])
 @login_required
 def new_blog():
-    
+
+
+
+
+
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+

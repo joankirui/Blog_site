f096e0a27c79d73000db1a5c69b8b881c6a4e86a
joankirui <joankirui99@gmail.com>
2021-11-13 18:16:55 +0300

    create a file upload form

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from flask_bootstrap import Bootstrap
 from flask import Flask
 from config import config_options
 from flask_login import LoginManager, login_manager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
+photos = UploadSet('photos',IMAGES)
 
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -18,6 +20,9 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+
+    # configure Uploadset
+    configure_uploads(app,photos)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://uninets:36573934@localhost/blog'
     SECRET_KEY = 'rv8O_BfuRi1WAQYHhhka0g'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     # email configurations
 

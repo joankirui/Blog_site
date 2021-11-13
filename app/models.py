cd6e0c7ba1b7430fc3d5c8daf8d6edc30a0c2ff2
joankirui <joankirui99@gmail.com>
2021-11-13 14:16:34 +0300

    securing passwords

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class Quotes:
     def __init__(self,author,id,quote):
@@ -15,11 +16,20 @@ class User(db.Model):
     pass_secure = db.Column(db.String(255))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    password_secure = db.Column(db.String(255))
     # relationships
-
     blog = db.relationship('Blog', backref='user', lazy='dynamic')
     comments = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'

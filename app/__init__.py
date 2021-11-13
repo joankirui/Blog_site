2b5244e4060df90b2268cc19423d45019080097f
joankirui <joankirui99@gmail.com>
2021-11-13 14:38:42 +0300

    include flask-login to help us manage the user authentication system

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask import Flask
 from config import config_options
+from flask_login import LoginManager, login_manager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)

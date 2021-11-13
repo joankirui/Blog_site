957049c2d00f52c8fb2f6ff8ce7f2c7f2c74e93a
joankirui <joankirui99@gmail.com>
2021-11-13 13:09:03 +0300

    create a class User

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask import Flask
+from config import config_options
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -11,4 +12,8 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
-    
+
+
+
+
+    return app

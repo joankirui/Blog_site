a636f8a810515b7d033c7019f044f68939e7d1d2
joankirui <joankirui99@gmail.com>
2021-11-13 12:53:32 +0300

    configure SQLAlchemy

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://uninets:36573934@localhost/blog'
+    SECRET_KEY = 'rv8O_BfuRi1WAQYHhhka0g'
+
+    # email configurations
+
+
+
+class ProdConfig(Config):
+    pass
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
